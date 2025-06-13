0eec01f26ff0ee3a6ec61b9ad974f6b0d74a0960
Vikas <vky2334@gmail.com>
2025-06-13 18:04:01 +0530

    todo app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ToDo from './Components/ToDo'
+
+const App = () => {
+  return (
+    <div className='bg-[#212121] text-white h-[100vh] w-[100%] flex items-center justify-center'>
+    <ToDo/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/ToDo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ToDo = () => {
+  return (
+    <div
+      className="bg-gray-700 text-white p-4 rounded-xl shadow-lg w-[90%]  max-w-md h-auto md:w-[500px] md:h-[350px] 
+                    flex flex-col  mx-auto">
+      
+      <h1 className="text-2xl font-bold text-center mt-2">To-Do List App</h1>
+    </div>
+  );
+};
+
+export default ToDo;
